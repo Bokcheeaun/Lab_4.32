16210dec5a18ed1d0d4c21dde9fcee1f5944be38
Bok <chee_aun97@hotmail.com>
2017-08-01 10:10:20 +0800

    done

---
+++ app/src/main/java/com/example/taruc/lab_43/AddActivity.java
@@ -0,0 +1,34 @@
+package com.example.taruc.lab_43;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddActivity extends AppCompatActivity {
+
+    EditText editTextName,editTextPhone,editTextEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextPhone = (EditText) findViewById(R.id.editTextPhone);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+
+    }
+    public void saveRecord(View view){ //Insert into database
+
+        UserRecord userRecord = new UserRecord();
+        userRecord.setPhone(editTextPhone.getText().toString());
+        userRecord.setName(editTextName.getText().toString());
+        userRecord.setEmail(editTextEmail.getText().toString());
+
+        UserSQLHelper userDataSource = new UserSQLHelper(this);
+        userDataSource.insertUser(userRecord);
+
+        this.finish(); //Terminate this Activity
+    }
+}
